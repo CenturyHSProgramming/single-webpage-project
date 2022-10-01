edab7d587ba4d893ef836d11d4c7d950adcae104
hundredvisionsguy <cwinikka@gmail.com>
2022-10-01 11:14:08 -0700

    adding test for existence of css

--- tests/test_css_requirements.py
+++ tests/test_css_requirements.py
@@ -4,7 +4,6 @@ Tests all CSS project requirements
 
 import pytest
 from webanalyst import report
-import webcode_tk.css as css
 
 path = "project/"
 
@@ -35,11 +34,25 @@ def get_css_general_results(data, description):
     return results
 
 
+def uses_css(data):
+    has_css = True
+    css_files = data.get("num_css_files")
+    try:
+        style_tag_data = data.get("style_tags")
+        has_style_tags = style_tag_data[0][1]
+    except Exception:
+        has_style_tags = False
+    if not css_files and not has_style_tags:
+        has_css = False
+    return has_css
+
+
 # Generate report for testing
 my_report = report.Report(path)
 my_report.generate_report()
 report_details = my_report.css_report.report_details
 standard_req_results = report_details['general_styles_goals']
+has_css = uses_css(report_details)
 standard_req_goals = get_css_goals(report_details['standard_requirements_goals'])
 general_results = get_css_general_results(standard_req_results,
                                           standard_req_goals)
@@ -49,3 +62,7 @@ general_results = get_css_general_results(standard_req_results,
 def test_for_general_css_requirements(description, results):
     print(description)
     assert results
+
+
+def test_css_validity():
+    assert has_css
