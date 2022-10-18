a299d551f3c5afeb7078231bbdfafe3cb6200e8d
hundredvisionsguy <winikkc@hsd.k12.or.us>
2022-10-17 17:00:49 -0700

    adjusting store-test results to make a pytest report

--- store_test_results.sh
+++ store_test_results.sh
@@ -3,15 +3,18 @@
 if [ -f report/test_results.txt ]; then
     echo "report/test_results.txt exists."
     echo "removing old results."
+
     rm report/test_results.txt
     echo "creating new pytest results."
     pytest --tb=no >> report/test_results.txt
+    echo "pytest results are saved in report/test_results.txt."
     exit 0
 else
     echo "creating new pytest results."
     pytest --tb=no >> report/test_results.txt
+    echo "pytest results are saved in report/test_results.txt."
     exit 0
 fi
 
-echo "Something went wrong...or did it?"
+echo "Something went wrong!"
 exit 1
