47bafc611d80d1663c21f5b01fae267e070d3d5f
hundredvisionsguy <winikkc@hsd.k12.or.us>
2022-11-09 12:01:07 -0800

    fixing store test results

--- store_test_results.sh
+++ store_test_results.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-NEW_TEST_RESULTS=$(pytest --tb=no | grep -v '====\|FAILED')
+NEW_TEST_RESULTS=$(pytest --tb=no | grep -v '====\|FAILED\|test_pytest_results.py')
 
 
 if [[ -f report/test_results.txt && -f report/old_test_results.txt ]]; then
     echo "both tests exist."
+    echo $NEW_TEST > report/test_results.txt
+    echo $OLD_TEST > report/old_test_results.txt
 elif [ -f report/test_results.txt ]; then
     echo "Copying new test as old test results (first time)"
     echo $NEW_TEST_RESULTS > report/old_test_results.txt
@@ -11,7 +13,7 @@ elif [ -f report/test_results.txt ]; then
 else
     echo "FAILURE: test results do not exist."
     echo "Creating new test results (report/test_results.txt)"
-    echo $NEW_TEST_RESULTS > report/test_results.txt
+    echo $NEW_TEST > report/test_results.txt
     exit 19
 fi
 
@@ -20,7 +22,7 @@ OLD_TEST=$(cat report/test_results.txt)
 # echo "Old test is as follows..."
 # echo $OLD_TEST
 
-if [ "$NEW_TEST_RESULTS" = "$OLD_TEST" ]; then
+if [ "$NEW_TEST" = "$OLD_TEST" ]; then
     echo "Test results are up to date!"
     exit 0
 else
