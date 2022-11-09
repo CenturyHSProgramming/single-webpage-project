63e8b3dd477084c2fc7fd8a4f75076b5d8ce48f5
hundredvisionsguy <winikkc@hsd.k12.or.us>
2022-11-08 16:14:41 -0800

    testing pytest to recreate the test results file

--- store_test_results.sh
+++ store_test_results.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 NEW_TEST_RESULTS=$(pytest --tb=no)
-echo NEW_TEST_RESULTS
+
 
 if [ -f report/test_results.txt ]; then
     echo "report/test_results.txt exists."
+    pytest --tb=no >> report/test_results.txt
 else
     echo "FAILURE: test results do not exist."
     echo "Creating new test results (report/test_results.txt)"
@@ -15,6 +16,7 @@ OLD_TEST_RESULTS=$(cat report/test_results.txt)
 
 if [ "$NEW_TEST_REQUIREMENTS" = "$OLD_TEST_REQUIREMENTS" ]; then
     echo "Test results are up to date!"
+    echo "$NEW_TEST_REQUIREMENTS" > report/test_results.txt
     exit 0
 else
     echo "FAILURE: report/test_results.txt is not up to date!"
