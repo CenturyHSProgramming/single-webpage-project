914fa5e4dc0a6f42335b522d7357bdf2c700da60
hundredvisionsguy <winikkc@hsd.k12.or.us>
2022-11-09 08:33:07 -0800

    adjusting store test results

--- store_test_results.sh
+++ store_test_results.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 NEW_TEST_RESULTS=$(pytest --tb=no | grep -v '====')
-echo $NEW_TEST_RESULTS
 
 
 if [ -f report/test_results.txt ]; then
@@ -8,19 +7,21 @@ if [ -f report/test_results.txt ]; then
 else
     echo "FAILURE: test results do not exist."
     echo "Creating new test results (report/test_results.txt)"
-    pytest --tb=no >> report/test_results.txt
+    echo $NEW_TEST_RESULTS > report/test_results.txt
     exit 19
 fi
 
 
-old_test_results=$(head -n 10 report/test_results.txt)
+old_test_results=$(report/test_results.txt)
+echo "Old test results are as follows..."
+echo $old_test_results
 
 if [ "$new_results_top" = "$old_test_results" ]; then
     echo "Test results are up to date!"
     exit 0
 else
-    echo "FAILURE: report/test_results.txt is not up to date!"
-    > report/test_results.txt
-    pytest --tb=no >> report/test_results.txt
+    #echo "FAILURE: report/test_results.txt is not up to date!"
+    #> report/test_results.txt
+    #pytest --tb=no >> report/test_results.txt
     exit 11
 fi
